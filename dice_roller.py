27cbef1624e8e71968e1fe5edde1955088e57df7
walliars <56570628+walliars@users.noreply.github.com>
2022-05-28 13:45:58 -0500

    adjusted line 5 roll variable to 6

--- dice_roller.py
+++ dice_roller.py
@@ -2,7 +2,7 @@ import random
 dice_rolls = 2
 dice_sum = 0
 for i in range(0,dice_rolls):
-  roll = random.randint(1,8)
+  roll = random.randint(1,6)
   dice_sum += roll
   if roll == 1:
     print(f'You rolled a {roll}! Critical Fail')
